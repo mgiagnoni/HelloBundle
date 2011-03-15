5981e7bb0cb71a07708b66578f46c0ea1e94d60c
mgiagnoni <gimassimo@gmail.com>
2011-03-15 15:35:43 +0100

    Added YAML definition for entity 'Friend'. Generated entity

---
+++ Entity/Friend.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace FooApps\HelloBundle\Entity;
+
+/**
+ * FooApps\HelloBundle\Entity\Friend
+ */
+class Friend
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var datetime $created_at
+     */
+    private $created_at;
+
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return datetime $createdAt
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+}
